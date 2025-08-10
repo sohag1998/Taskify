31313f3252ce731359e3caa14d1493541d8b492d
SOHAG-HOSSAIN\sohag <sohaghossainice@gmail.com>
2025-08-10 23:04:33 +0600

    Add Employee Controller and Employee model.

---
+++ src/Taskify/Taskify.Web/Models/Employee.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Taskify.Web.Models
+{
+    public class Employee
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string? Name { get; set; }
+
+        [Required]
+        public string? Email { get; set; }
+
+        [Required]
+        public string? Phone { get; set; }
+
+        [Required]
+        public string? Gender { get; set; }
+
+        [Required]
+        public string? PresentAddress { get; set; }
+        public string? PermanentAdress { get; set; }
+
+        [Required]
+        public string? ProfileImageUrl { get; set; }
+
+        [Required]
+        public string? University { get; set; }
+
+    }
+}
